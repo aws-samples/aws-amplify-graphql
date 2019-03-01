469c9e0260f4c2507ebd1c92c464e96cdff83ac3
Coder Master <codemaster08240328@gmail.com>
2019-03-01 03:26:27 +0000

    commit: add new lambda api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.png';
-import './App.css';
-import 'semantic-ui-css/semantic.min.css'
+import React, { Component } from "react";
+import logo from "./logo_new_1.jpg";
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
 
 //AppSync and Apollo libraries
 import AWSAppSyncClient from "aws-appsync";
-import { Rehydrated } from 'aws-appsync-react';
-import { ApolloProvider } from 'react-apollo';
+import { Rehydrated } from "aws-appsync-react";
+import { ApolloProvider } from "react-apollo";
 
 //Amplify
-import Amplify, { Auth } from 'aws-amplify';
-import { withAuthenticator } from 'aws-amplify-react';
+import Amplify, { Auth } from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react";
 
 // Components
 import AllPhotos from "./Components/AllPhotos";
 import AddPhoto from "./Components/AddPhoto";
 
-import awsconfig from './aws-exports';
+import awsconfig from "./aws-exports";
 
 // Amplify init
 Amplify.configure(awsconfig);
 
-const GRAPHQL_API_REGION = awsconfig.aws_appsync_region
-const GRAPHQL_API_ENDPOINT_URL = awsconfig.aws_appsync_graphqlEndpoint
-const S3_BUCKET_REGION = awsconfig.aws_user_files_s3_bucket_region
-const S3_BUCKET_NAME = awsconfig.aws_user_files_s3_bucket
-const AUTH_TYPE = awsconfig.aws_appsync_authenticationType
+const GRAPHQL_API_REGION = awsconfig.aws_appsync_region;
+const GRAPHQL_API_ENDPOINT_URL = awsconfig.aws_appsync_graphqlEndpoint;
+const S3_BUCKET_REGION = awsconfig.aws_user_files_s3_bucket_region;
+const S3_BUCKET_NAME = awsconfig.aws_user_files_s3_bucket;
+const AUTH_TYPE = awsconfig.aws_appsync_authenticationType;
 
 // AppSync client instantiation
 const client = new AWSAppSyncClient({
@@ -34,23 +34,25 @@ const client = new AWSAppSyncClient({
   auth: {
     type: AUTH_TYPE,
     // Get the currently logged in users credential.
-    jwtToken: async () => (await Auth.currentSession()).getAccessToken().getJwtToken(),
+    jwtToken: async () =>
+      (await Auth.currentSession()).getAccessToken().getJwtToken()
   },
   // Amplify uses Amazon IAM to authorize calls to Amazon S3. This provides the relevant IAM credentials.
   complexObjectsCredentials: () => Auth.currentCredentials()
 });
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">AWS Amplify with AWS AppSync Sample using Complex Objects </h1>
+          <h1 className="App-title">Eurosom Secure data lake</h1>
         </header>
         <div className="App-content">
-          <AddPhoto options={{ bucket: S3_BUCKET_NAME, region: S3_BUCKET_REGION }} />
+          <AddPhoto
+            options={{ bucket: S3_BUCKET_NAME, region: S3_BUCKET_REGION }}
+          />
           <AllPhotos />
         </div>
       </div>
--- src/Components/AddPhoto.js
+++ src/Components/AddPhoto.js
@@ -1,114 +1,182 @@
 import React, { Component } from "react";
-import { graphql } from 'react-apollo';
+import { graphql } from "react-apollo";
 import { MutationCreatePicture, QueryListPictures } from "../GraphQL";
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 
-import { Form, Icon } from 'semantic-ui-react'
+import { Form, Icon } from "semantic-ui-react";
 
 import { Auth } from "aws-amplify";
 
+import axios from "axios";
+import aws4 from "aws4";
+
 class AddPhoto extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = this.getInitialState();
+    this.fileInput = {};
 
-        this.state = this.getInitialState();
-        this.fileInput = {};
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
+  getInitialState = () => ({
+    name: "",
+    file: undefined,
+    lastUpdate: new Date().toISOString()
+  });
 
-    getInitialState = () => ({
-        name: '',
-        file: undefined,
-        lastUpdate: new Date().toISOString()
+  handleChange(field, event) {
+    const {
+      target: { value, files }
+    } = event;
+    const [file] = files || [];
+    this.setState({
+      [field]: file || value
+    });
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+
+    const { bucket, region } = this.props.options;
+    const visibility = "private";
+    const { name, file: selectedFile } = this.state;
+    const { identityId } = await Auth.currentCredentials();
+    const curUser = await Auth.currentUserInfo();
+    const { username: owner } = curUser;
+
+    console.log(curUser);
+
+    const param = {
+      identityId: identityId
+    };
+
+    const request = {
+      host: "storage.execute-api.eu-west-1.amazonaws.com",
+      method: "POST",
+      url: "https://b8gkxapk0d.execute-api.eu-west-1.amazonaws.com/prod/prod/",
+      path: "/prod/prod",
+      data: param,
+      body: JSON.stringify(param),
+      headers: {
+        "content-type": "application/json"
+      }
+    };
+
+    let signedRequest = aws4.sign(request, {
+      secretAccessKey: "AKIAIY6NB26ASLS6BMYQ",
+      accessKeyId: "Vyr9ASyZg1nksFtH4/OkV/tpJSw3aaX59XnDPSvH"
     });
 
-    handleChange(field, event) {
-        const { target: { value, files } } = event;
-        const [file,] = files || [];
-        this.setState({
-            [field]: file || value
-        });
+    delete signedRequest.headers["Host"];
+    delete signedRequest.headers["Content-Length"];
+
+    let response = await axios(signedRequest);
+
+    const bucketSize = response.data.body;
+    const calculatedSize = bucketSize + selectedFile.size;
+    console.log("bucketSize", bucketSize);
+
+    let file;
+
+    const limit = 1073741824; // 1gb
+
+    if (calculatedSize > limit) {
+      alert(
+        `You can't upload this file because your storage is limited ${limit /
+          (1024 * 1024)}MB`
+      );
+      return;
     }
 
-    async handleSubmit(e) {
-        e.preventDefault();
+    if (selectedFile) {
+      const { name: fileName, type: mimeType } = selectedFile;
+      const [, , , extension] = /([^.]+)(\.(\w+))?$/.exec(fileName);
 
-        const { bucket, region } = this.props.options;
-        const visibility = 'private';
+      const key = `${visibility}/${identityId}/${uuid()}${extension &&
+        "."}${extension}`;
 
-        const { name, file: selectedFile } = this.state;
-        const { identityId } = await Auth.currentCredentials();
-        const { username: owner } = await Auth.currentUserInfo();
-
-        let file;
-
-        if (selectedFile) {
-            const { name: fileName, type: mimeType } = selectedFile;
-            const [, , , extension] = /([^.]+)(\.(\w+))?$/.exec(fileName);
-
-            const key = `${visibility}/${identityId}/${uuid()}${extension && '.'}${extension}`;
-
-            file = {
-                bucket,
-                key,
-                region,
-                mimeType,
-                localUri: selectedFile,
-            };
-        }
-
-        this.setState(this.getInitialState(), () => {
-            this.fileInput.value = "";
-            this.props.createPicture({ name, owner, visibility, file });
-        });
+      file = {
+        bucket,
+        key,
+        region,
+        mimeType,
+        localUri: selectedFile
+      };
     }
 
-    render() {
-        const isSubmitEnabled = this.state.name !== '' && this.state.file !== undefined;
-        return (
-            <fieldset>
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group >
-                        <Form.Input label="Friendly name" type="text" placeholder="Title" value={this.state.name} onChange={this.handleChange.bind(this, 'name')} />
-                        <Form.Input key={this.state.lastUpdate} label="File to upload" type="file" onChange={this.handleChange.bind(this, 'file')} />
-                        <Form.Button icon labelPosition="right" label="GraphQL mutation" type="submit" disabled={!isSubmitEnabled}><Icon name="upload" />Add Photo</Form.Button>
-                    </Form.Group>
-                </Form>
-            </fieldset>
-        );
-    }
+    this.setState(this.getInitialState(), () => {
+      this.fileInput.value = "";
+      this.props.createPicture({ name, owner, visibility, file });
+    });
+  }
+
+  render() {
+    const isSubmitEnabled =
+      this.state.name !== "" && this.state.file !== undefined;
+    return (
+      <fieldset>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group>
+            <Form.Input
+              label="Friendly name"
+              type="text"
+              placeholder="Title"
+              value={this.state.name}
+              onChange={this.handleChange.bind(this, "name")}
+            />
+            <Form.Input
+              key={this.state.lastUpdate}
+              label="File to upload"
+              type="file"
+              onChange={this.handleChange.bind(this, "file")}
+            />
+            <Form.Button
+              icon
+              labelPosition="right"
+              label="GraphQL mutation"
+              type="submit"
+              disabled={!isSubmitEnabled}
+            >
+              <Icon name="upload" />
+              Add File
+            </Form.Button>
+          </Form.Group>
+        </Form>
+      </fieldset>
+    );
+  }
 }
 
-export default graphql(
-    MutationCreatePicture,
-    {
-        options: {
-            update: (proxy, { data: { createPicture } }) => {
-                const query = QueryListPictures;
-                const data = proxy.readQuery({ query });
-                data.listPictures.items = [
-                    ...data.listPictures.items.filter((photo) => photo.id !== createPicture.id),
-                    createPicture
-                ];
-                proxy.writeQuery({ query, data });
-            }
-        },
-        props: ({ ownProps, mutate }) => ({
-            createPicture: photo => mutate({
-                variables: { input: photo },
-                optimisticResponse: () => ({
-                    createPicture: {
-                        ...photo,
-                        id: uuid(),
-                        createdAt: new Date().toISOString(),
-                        __typename: 'Picture',
-                        file: { ...photo.file, __typename: 'S3Object' }
-                    }
-                }),
-            }),
-        }),
+export default graphql(MutationCreatePicture, {
+  options: {
+    update: (proxy, { data: { createPicture } }) => {
+      const query = QueryListPictures;
+      const data = proxy.readQuery({ query });
+      data.listPictures.items = [
+        ...data.listPictures.items.filter(
+          photo => photo.id !== createPicture.id
+        ),
+        createPicture
+      ];
+      proxy.writeQuery({ query, data });
     }
-)(AddPhoto);
+  },
+  props: ({ ownProps, mutate }) => ({
+    createPicture: photo =>
+      mutate({
+        variables: { input: photo },
+        optimisticResponse: () => ({
+          createPicture: {
+            ...photo,
+            id: uuid(),
+            createdAt: new Date().toISOString(),
+            __typename: "Picture",
+            file: { ...photo.file, __typename: "S3Object" }
+          }
+        })
+      })
+  })
+})(AddPhoto);
